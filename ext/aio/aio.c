de794838084f1da9270e093b7c71dc9ddc69aad6
Lourens Naudé <lourens@methodmissing.com>
2009-07-26 18:20:09 +0100

    New control block based API ftw!

--- ext/aio/aio.c
+++ ext/aio/aio.c
@@ -53,12 +53,6 @@ typedef struct{
 	VALUE io; 
 } rb_aiocb_t;
 
-/* Allows for passing n-1 arguments to the first rb_ensure function call */
-struct aio_read_multi_args {
-	aiocb_t **list;
-	int reads; 
-};
-
 static void rb_aio_error( char * msg ){
     rb_raise( eAio, msg );
 }
@@ -318,7 +312,7 @@ static void rb_aio_read_multi_error(){
     switch(errno){
        case EAGAIN: 
 	        rb_aio_error( "Resources necessary to queue all the requests are not available at the moment." );
-		  break;
+		    break;
        case EIO: 
 	        rb_aio_error( "One or more requests failed" );
 			break;
@@ -331,100 +325,53 @@ static void rb_aio_read_multi_error(){
 /*
  *  Initiates lio_listio
  */
-static VALUE rb_aio_read_multi( struct aio_read_multi_args *args ){
+static VALUE rb_aio_lio_listio( VALUE *cbs ){
 	int op, ret;
-    VALUE results = rb_ary_new2( args->reads );
+	aiocb_t *list[AIO_MAX_LIST];
+	int reads = RARRAY_LEN(cbs);
 	
+    bzero( (char *)list, sizeof(list) );	
+    VALUE results = rb_ary_new2( reads );
+    for (op=0; op < reads; op++) {		
+	    rb_aiocb_t *cb = GetCBStruct(RARRAY_PTR(cbs)[op]);
+		list[op] = &cb->cb;       
+    }
 	TRAP_BEG;
-    ret = lio_listio( LIO_WAIT, args->list, args->reads, NULL );
+    ret = lio_listio( LIO_WAIT, &list, reads, NULL );
 	TRAP_END;
 	if (ret != 0) rb_aio_read_multi_error();
-    for (op=0; op < args->reads; op++) {
-		rb_ary_push( results, rb_tainted_str_new( (char *)(*args->list)[op].aio_buf, (*args->list)[op].aio_nbytes ) );
-    }
+    for (op=0; op < reads; op++) {
+		rb_ary_push( results, rb_tainted_str_new( (char *)list[op]->aio_buf, list[op]->aio_nbytes ) );
+    } 
 	return results;
 }
 
 /*
- *  Helper to ensure files opened via AIO.read_multi is closed.
+ *  Helper to ensure files opened via AIO.lio_listio is closed.
  */
-static void rb_io_closes( VALUE ios ){
+static void rb_io_closes( VALUE cbs ){
     int io;
-
-    for (io=0; io < RARRAY_LEN(ios); io++) {
-  	  rb_io_close( RARRAY_PTR(ios)[io] );
-    } 
-}
-
-/*
- *  Setup a AIO control block for the given file descriptor and number of bytes to read.
- *  Defaults to reading the entire file size into the buffer.Granular offsets would be 
- *  supported in a future version.
- */
-static void rb_setup_aio_cb( aiocb_t *cb, int *fd, int *length ){
-    bzero(cb, sizeof(aiocb_t));
-
-	cb->aio_buf = malloc(*length + 1);
-	if (!cb->aio_buf) rb_aio_error( "not able to allocate a read buffer" );
-
-	cb->aio_fildes = *fd;
-	cb->aio_nbytes = *length;
-	cb->aio_offset = 0;
-	cb->aio_sigevent.sigev_notify = SIGEV_NONE;
-	cb->aio_sigevent.sigev_signo = 0;
-	cb->aio_sigevent.sigev_value.sival_int = 0;
-	cb->aio_lio_opcode = LIO_READ;
-}
-
-/*
- *  Open file for reading and infer file descriptor and the size to read. 
- */
-static VALUE rb_aio_open_file( VALUE *file, int *fd, int *length ){ 
-#ifdef HAVE_TBR
-	rb_io_t *fptr;
-#else	
-	OpenFile *fptr;
-#endif
-    VALUE io;
-
-    struct stat stats;
-   
-    Check_Type(*file, T_STRING);	
-
-    io = rb_file_open(RSTRING_PTR(*file), "r");
-    GetOpenFile(io, fptr);
-    rb_io_check_readable(fptr); 	
-
-#ifdef HAVE_TBR
-    *fd = fptr->fd;
-#else	
-    *fd = fileno(fptr->f);
-#endif
-    fstat(*fd, &stats);
-    *length = stats.st_size;
-	return io;
+    for (io=0; io < RARRAY_LEN(cbs); io++) {
+	  control_block_close( RARRAY_PTR(cbs)[io] );
+    }  
 }
 
 /*
  *  call-seq:
- *     AIO.read('file1') -> string
+ *     AIO.read(cb) -> string
  *  
  *  Asynchronously reads a file.This is an initial *blocking* implementation until
  *  cross platform notification is supported.
  */
-static VALUE rb_aio_s_read( VALUE aio, VALUE file ){
-    int fd, length; 
-    aiocb_t cb;
-	 
-	VALUE io = rb_aio_open_file( &file, &fd, &length );
-	rb_setup_aio_cb( &cb, &fd, &length );
-
-    return rb_ensure( rb_aio_read, (VALUE)&cb, rb_io_close, io );
+static VALUE 
+rb_aio_s_read( VALUE aio, VALUE cb ){
+    rb_aiocb_t *cbs = GetCBStruct(cb);
+    return rb_ensure( rb_aio_read, (VALUE)&cbs->cb, control_block_close, cb );
 }
 
 /*
  *  call-seq:
- *     AIO.read_multi('file1','file2', ...) -> array
+ *     AIO.lio_listio(cb1, cb2, ...) -> array
  *  
  *  Schedules a batch of read requests for execution by the kernel in order
  *  to reduce system calls.Blocks until all the requests complete and returns
@@ -444,28 +391,10 @@ static VALUE rb_aio_s_read( VALUE aio, VALUE file ){
  *  close_nocancel(0x4)	 = 0 0
  *  close_nocancel(0x3)	 = 0 0
  */
-static VALUE rb_aio_s_read_multi( VALUE aio, VALUE files ){
-    int op, fd, length; 
-    struct aio_read_multi_args args;
-	aiocb_t cb[AIO_MAX_LIST];
-	aiocb_t *list[AIO_MAX_LIST]; 
-
-	int reads = RARRAY_LEN(files);
-	if (reads > AIO_MAX_LIST) rb_aio_error( "maximum number of I/O calls exceeded!" );
-	VALUE ios = rb_ary_new2( reads );
-
-    bzero( (char *)list, sizeof(list) );
-    for (op=0; op < reads; op++) {
-
-	  rb_ary_push( ios, rb_aio_open_file( &RARRAY_PTR(files)[op], &fd, &length ) );
-
-      rb_setup_aio_cb( &cb[op], &fd, &length );
-      list[op] = &cb[op];       
-    }
-
-	args.list = list;
-	args.reads = reads;
-    return rb_ensure( rb_aio_read_multi, (VALUE)&args, rb_io_closes, ios );
+static VALUE rb_aio_s_lio_listio( VALUE aio, VALUE cbs ){
+	int reads = RARRAY_LEN(cbs);
+	if (reads > AIO_MAX_LIST) rb_aio_error( "maximum number of AIO calls exceeded!" );
+	return rb_ensure( rb_aio_lio_listio, (VALUE)cbs, rb_io_closes, cbs );
 }
 
 void Init_aio()
@@ -499,7 +428,7 @@ void Init_aio()
 
     eAio = rb_define_class_under(mAio, "Error", rb_eStandardError);
 
-    rb_define_module_function( mAio, "read_multi", rb_aio_s_read_multi, -2 );
+    rb_define_module_function( mAio, "lio_listio", rb_aio_s_lio_listio, -2 );
     rb_define_module_function( mAio, "read", rb_aio_s_read, 1 );
 
 }
--- test/test_aio.rb
+++ test/test_aio.rb
@@ -1,19 +1,25 @@
 require File.dirname(__FILE__) + '/helper'
 
 class TestAio < Test::Unit::TestCase
-  
+
   def test_read_multi
-    assert_equal %w(one two three four), AIO.read_multi( *fixtures( *%w(1.txt 2.txt 3.txt 4.txt) ) )
+    cbs = fixtures( *%w(1.txt 2.txt 3.txt 4.txt) ).map{|f| AIO::CB.new(f) }
+    cbs.each{|cb| assert cb.open? }
+    assert_equal %w(one two three four), AIO.lio_listio( *cbs )
+    cbs.each{|cb| assert !cb.open? }
   end
 
   def test_read_multi_limits
     assert_raises AIO::Error do
-      AIO.read_multi( *fixtures( *(['file'] * 17)) )
+      AIO.lio_listio( *fixtures( *([AIO::CB.new] * 17)) )
     end  
   end 
-   
+
   def test_read
-    assert_equal 'one', AIO.read( fixtures( '1.txt' ).first )
+    cb = AIO::CB.new(fixtures( '1.txt' ).first)
+    assert cb.open?
+    assert_equal 'one', AIO.read( cb )
+    assert !cb.open?
   end
   
 end
