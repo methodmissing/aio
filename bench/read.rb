37320205ac067171aa40e7efe63e9bc80465e3f4
Lourens Naudé <lourens@methodmissing.com>
2009-07-26 19:59:23 +0100

    Benchmarks for non-blocking lio_listio

--- bench/read.rb
+++ bench/read.rb
@@ -2,10 +2,16 @@ require File.dirname(__FILE__) + '/../ext/aio/aio'
 require "benchmark"
 
 CALLBACKS = (1..8).to_a.map{|f| AIO::CB.new( File.dirname(__FILE__) + "/../test/fixtures/#{f}.txt" ) }
+NON_BLOCKING = [AIO::NOWAIT].concat( CALLBACKS )
 PATHS = CALLBACKS.map{|cb| cb.path }
 
 Benchmark.bm do |results|
-  results.report("AIO.lio_listio") { AIO.lio_listio( *CALLBACKS ) }  
+  results.report("AIO.lio_listio(AIO::WAIT)") { AIO.lio_listio( *CALLBACKS ) }  
+  results.report("IO.read") { PATHS.each{|p| IO.read(p) } }  
+end
+
+Benchmark.bm do |results|
+  results.report("AIO.lio_listio(AIO::NOWAIT)") { AIO.lio_listio( *NON_BLOCKING ) }  
   results.report("IO.read") { PATHS.each{|p| IO.read(p) } }  
 end
 
