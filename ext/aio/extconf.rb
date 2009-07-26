93ccd4e477c231041a247a78166d0e8bf3a09e11
Lourens Naudé <lourens@methodmissing.com>
2009-07-26 19:13:07 +0100

    Restrict rt lib check to Linux only

--- ext/aio/extconf.rb
+++ ext/aio/extconf.rb
@@ -6,7 +6,9 @@ end
 
 dir_config('aio')
 
-#raise 'cannot find AIO' unless have_library('rt', 'aio_read', 'aio.h')
+if RUBY_PLATFORM =~ /linux/i
+  raise 'cannot find AIO' unless have_library('rt', 'aio_read', 'aio.h')
+end
 add_define 'HAVE_TBR' if have_func('rb_thread_blocking_region') and have_macro('RUBY_UBF_IO', 'ruby.h')
 add_define 'RUBY18' if have_var('rb_trap_immediate', ['ruby.h', 'rubysig.h'])
 
